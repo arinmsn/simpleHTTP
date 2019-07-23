a2427941cb357bfd8202b61a0bb6dc223abfd187
arinmsn <arinmsn@gmail.com>
2019-07-23 15:26:46 -0700

    GET and PUT are working

---
+++ app.js
@@ -0,0 +1,44 @@
+// Instantiate simpleHTTP
+const http = new simpleHTTP;
+
+// http.get('https://jsonplaceholder.typicode.com/posts', function(err, posts) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(posts);
+//     }
+// });
+
+// POST request
+// Create a Post
+
+const data = {
+    title: 'Custom Post',
+    body: 'This is a custom post'
+};
+
+// http.post('https://jsonplaceholder.typicode.com/posts', data, function(err, post){
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Update (PUT) post
+// http.put('https://jsonplaceholder.typicode.com/posts/1', data, function(err, post) {
+//     if(err) {
+//         console.log(err);
+//     } else {
+//         console.log(post);
+//     }
+// });
+
+// Delete Request
+http.get('https://jsonplaceholder.typicode.com/posts', function(err, response) {
+    if(err) {
+        console.log(err);
+    } else {
+        console.log(posts);
+    }
+});
---
+++ simpleHttp.js
@@ -0,0 +1,68 @@
+/*
+    Library will be utilitzed using ES5 - Prototypes
+*/
+
+function simpleHTTP() {
+    this.http = new XMLHttpRequest();
+}
+
+simpleHTTP.prototype.get = function(url, callback) {
+    this.http.open('GET', url, true);
+    let self = this;
+    this.http.onload = function() {
+        // this.http.status won't work
+        // TypeError: Cannot read property 'status' of undefined
+        // We can use 'self' or => instead of function() as well
+        if(self.http.status === 200) {
+            // null is the error parameter
+            callback(null, self.http.responseText);
+        } else {
+            callback(`Error:  ${self.http.status}`);
+        }
+    }
+
+    this.http.send();
+}
+
+simpleHTTP.prototype.post = function(url, data, callback) {
+    this.http.open('POST', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
+
+simpleHTTP.prototype.put = function(url, data, callback) {
+    this.http.open('PUT', url, true);
+    this.http.setRequestHeader('Content-type', 'application/json');
+
+    let self = this;
+    this.http.onload = function() {
+        callback(null, self.http.responseText);
+    }
+
+    this.http.send(JSON.stringify(data));
+}
+
+simpleHTTP.prototype.delete = function(url, callback) {
+    this.http.open('DELETE', url, true);
+    let self = this;
+    this.http.onload = function() {
+        // this.http.status won't work
+        // TypeError: Cannot read property 'status' of undefined
+        // We can use 'self' or => instead of function() as well
+        if(self.http.status === 200) {
+            // null is the error parameter
+            callback(null, 'Post deleted');
+        } else {
+            callback(`Error:  ${self.http.status}`);
+        }
+    }
+
+    this.http.send();
+}

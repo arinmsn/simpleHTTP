0c0c18a94abf54804f05b150c8f1fcd742d4a1ff
Arin <arinmsn@gmail.com>
2019-07-23 18:15:23 -0700

    http-delete works. Promises example is shown inside app-js file

--- app.js
+++ app.js
@@ -35,10 +35,10 @@ const data = {
 // });
 
 // Delete Request
-http.get('https://jsonplaceholder.typicode.com/posts', function(err, response) {
+http.delete('https://jsonplaceholder.typicode.com/posts/1', function(err, response) {
     if(err) {
         console.log(err);
     } else {
-        console.log(posts);
+        console.log(response);
     }
 });
